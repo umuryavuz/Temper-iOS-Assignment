5fea5c80311c5d8e00b9a798e8003a3268d3a52b
Umur Burhan Yavuz <140215925+umuryavuzdeste@users.noreply.github.com>
2023-09-30 22:51:57 +0200

    Initial Commit

---
+++ Temper iOS Assignment/Temper_iOS_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Temper_iOS_AssignmentApp.swift
+//  Temper iOS Assignment
+//
+//  Created by Umur Yavuz on 30/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Temper_iOS_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
